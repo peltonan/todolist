f1685c023635bc8c4e32326d9b5aeb2add32be1a
peltonan <peltonen.annette@gmail.com>
2022-07-27 16:43:31 +0300

    Working on Confetti function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,22 @@
 import { useState } from 'react'
 import Todo from './Todo'
 import TodoForm from './TodoForm'
+import Confetti from 'react-dom-confetti';
+
+const config = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 70,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: "10px",
+    height: "10px",
+    perspective: "500px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
 
 function TodoList() {
     const [todos, setTodos] = useState([])
@@ -49,6 +65,7 @@ function TodoList() {
                 removeTodo={removeTodo}
                 updateTodo={updateTodo}
             />
+            {/*  <Confetti active={todo.isComplete} config={config} /> */}
         </div>
     )
 }
